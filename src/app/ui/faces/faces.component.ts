790fb27714e00d9a6f832c2cfdf1e92b826d106d
Igor Iwaniak <igornocny@gmail.com>
2022-09-06 21:13:29 +0200

    feat:task 5

---
+++ src/app/ui/faces/faces.component.ts
@@ -0,0 +1,19 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { Observable } from 'rxjs';
+import { EmployeeService } from '../../services/employee.service';
+import { PersonModel } from '../../model/person.model';
+
+@Component({
+  selector: 'app-faces',
+  templateUrl: './faces.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FacesComponent {
+  data$: Observable<PersonModel[]> = this._employeeService.getAll();
+  constructor(private _employeeService: EmployeeService) {}
+}
