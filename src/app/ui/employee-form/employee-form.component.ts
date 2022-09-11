61a595c456a6e80170f83fe724ff65dee2377cca
Igor Iwaniak <igornocny@gmail.com>
2022-09-11 19:09:37 +0200

    feat:task 2.2 display alert

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -13,10 +13,22 @@ import { FormControl, FormGroup, Validator, Validators } from '@angular/forms';
 })
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup({
-    name: new FormControl('Anna', [Validators.required]),
-    age: new FormControl('25', [Validators.min(18), Validators.required]),
-    salary: new FormControl('5000', [Validators.required]),
+    name: new FormControl(null, [Validators.required]),
+    age: new FormControl(null, [Validators.min(18), Validators.required]),
+    salary: new FormControl(null, [Validators.required]),
   });
+
+  onButtonClicked(form: { name: string; age: string; salary: string }) {
+    alert(
+      'user was successfully added to the database  ' +
+        'Name:' +
+        form.name +
+        'Age:' +
+        form.age +
+        'Salary:' +
+        form.salary
+    );
+  }
 
   onEmployeeFormSubmitted(employeeForm: FormGroup): void {}
 }
