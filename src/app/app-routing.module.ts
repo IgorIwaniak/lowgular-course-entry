afc0d09d043b5903a4eb8307c19f4e47244cb2fa
Igor Iwaniak <igornocny@gmail.com>
2022-11-02 13:41:07 +0100

    feat: add Dynamic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './ui/employee-list/employee-list.component';
 import { EmployeeFormComponent } from './ui/employee-form/employee-form.component';
+import { EmployeeDetailsComponent } from './ui/employee-details/employee-details.component';
 import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
 import { EmployeeServiceModule } from './services/employee.service-module';
 import { EmployeeFormComponentModule } from './ui/employee-form/employee-form.component-module';
 import { FooterComponentModule } from './ui/footer/footer.component-module';
 import { NavbarComponentModule } from './ui/navbar/navbar.component-module';
+import { EmployeeDetailsComponentModule } from './ui/employee-details/employee-details.component-module';
 
 const routes: Routes = [
   { path: 'employee-list', component: EmployeeListComponent },
   { path: 'create-employee', component: EmployeeFormComponent },
+  { path: 'employee-details/:id', component: EmployeeDetailsComponent }
 ];
 
 @NgModule({
@@ -21,7 +24,8 @@ const routes: Routes = [
     EmployeeFormComponentModule,
     FooterComponentModule,
     NavbarComponentModule,
+    EmployeeDetailsComponentModule
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
